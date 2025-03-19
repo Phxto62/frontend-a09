4ffed071f1cea83fcfae35fe60e5e3b8fe6de1bf
Phxto62 <realphxto@gmail.com>
2025-03-19 18:31:47 +0700

    idk

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -1,6 +1,6 @@
 import { AuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
-import userLogIn from "@/libs/userLogIn"
+import userLogIn from "@/libs/userLogin"
 
 export const authOptions:AuthOptions = {
     providers: [
